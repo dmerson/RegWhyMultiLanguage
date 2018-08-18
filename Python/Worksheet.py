3e33bed31c10391d5df911fba6c4527dd8adee4c
don-E Merson <dmerson@email.arizona.edu>
2018-08-17 21:16:48 -0700

    Filled out some more of Python. Can't do RegWhy.Group.Start.(anything) had to retool.

--- Python/Worksheet.py
+++ Python/Worksheet.py
@@ -21,18 +21,21 @@ class RegWhy:
     returnString="\r"
     newLine="\n"
     formFeed="\f"
+    orMarker="|"
     def __init__(self):
         self.CharacterType = self.CharacterType()
         self.Group = self.Group()
         self.Where=self.Where()
         self.Count=self.Count()
-
-    def Statement(self,listOfRegWhy):
+    @staticmethod
+    def Statement(listOfRegWhy):
             final_string=""
             for str in listOfRegWhy:
                 final_string +=str
             return final_string
-
+    @staticmethod
+    def Literal(value):
+            return value
     class CharacterType:
         anyCharacter="."
         digit ="\d"
@@ -47,15 +50,53 @@ class RegWhy:
         upperCaseASCII="[A-Z]"
         anyASCII='[ -~]'
         endOfFile="^Z"
+        @staticmethod
+        def unicode(fourDigitUniCodeNumber):
+            unicodeString ="\\u" + fourDigitUniCodeNumber
+            return (unicodeString)
         def __init__(self):
             pass
     class Group:
+        startCapturing="("
+        startNonCapturing="(:"
+        end=")"
+        endOptional=")?"
+        endZeroOrMore=")*"
+        endOneOfMore=")+"
+        smallestMatch="?"
+        largestMatch=""
+        endOptionSmallestMatch=")??"
+        endZeroOrMoreSmallestMatch=")*?"
+        endOneOrMoreSmallestMatch=")+?"
+        endOfNumberedSmallestMatch="?"
+        lastMatch="$&"
+        lastParen="$+"
+        precedingMatch ="%`"
         def __init__(self):
             pass
+
     class Where:
+        startOfString="^"
+        endOfString="$"
         def __init__(self):
             pass
     class Count:
+        optional="?"
+        zeroOrMore="*"
+        oneOrMore="+"
+
+        @staticmethod
+        def exactNumber(numberOfDigits):
+            return "{" + str(numberOfDigits) + "}"
+        
+        @staticmethod
+        def exactNumberOrMore(numberOfDigits):
+            return "{" + str(numberOfDigits) + ",}"
+
+        @staticmethod
+        def rangeOfTimes(bottomNumber,topNumber):
+            return "{" + str(bottomNumber) + "," + str(topNumber) + "}"
+        
         def __init__(self):
             pass
     class Do:
@@ -69,3 +110,14 @@ class RegWhy:
 if __name__ == '__main__':
     print(RegWhy.asterik)
     print(RegWhy.CharacterType.anyCharacter)
+    print(RegWhy.Group.end)
+    print(RegWhy.Statement([
+        RegWhy.Group.startCapturing,
+        RegWhy.Literal("and"),
+        RegWhy.orMarker,
+        RegWhy.Literal("or"),
+        RegWhy.Group.end,
+        RegWhy.CharacterType.anyCharacter,
+        RegWhy.CharacterType.unicode("1234"),
+        RegWhy.Count.exactNumber(3)
+    ]))

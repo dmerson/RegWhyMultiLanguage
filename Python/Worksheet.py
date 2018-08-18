dbb52d03bb308a8229e69b9daf099bb81ce2c757
don-E Merson <dmerson@email.arizona.edu>
2018-08-17 20:28:11 -0700

    Filling in the class for Python start.

--- Python/Worksheet.py
+++ Python/Worksheet.py
@@ -1,30 +1,71 @@
 #!/usr/bin/env python
 
 class RegWhy:
-
+    period="\."
+    backSlash="\\"
+    bar ="\|"
+    leftParenthesis="\("
+    rightParenthesis="\)"
+    nullString="\0"
+    leftBracket="\["
+    rightBracket="\]"
+    leftBrace="\{"
+    rightBrace="\}"
+    dollarSign="\$"
+    asterik="\*"
+    questionMark="\?"
+    leftAngle="\<"
+    rightAngle="\>"
+    caret="\^"
+    tab="\t"
+    returnString="\r"
+    newLine="\n"
+    formFeed="\f"
     def __init__(self):
-        self.name = 'Guido'
-        self.Make = self.Make()
+        self.CharacterType = self.CharacterType()
+        self.Group = self.Group()
+        self.Where=self.Where()
+        self.Count=self.Count()
 
-    class Make:
-        def __init__(self):
-            self.Group=self.Group()
-        def talk(self):
-            return 'talking...'
-        def Statement(self,listOfRegWhy):
+    def Statement(self,listOfRegWhy):
             final_string=""
             for str in listOfRegWhy:
                 final_string +=str
             return final_string
-        class Group:
-            def range(self):
-                return "["
+
+    class CharacterType:
+        anyCharacter="."
+        digit ="\d"
+        nonDigit="\D"
+        whiteSpace="\s"
+        nonWhiteSpace="\S"
+        wordCharacter="\w"
+        nonWordCharacter="\W"
+        wordBounday="\b"
+        nonWordBoundary="\B"
+        lowerCaseASCI="[a-z]"
+        upperCaseASCII="[A-Z]"
+        anyASCII='[ -~]'
+        endOfFile="^Z"
+        def __init__(self):
+            pass
+    class Group:
+        def __init__(self):
+            pass
+    class Where:
+        def __init__(self):
+            pass
+    class Count:
+        def __init__(self):
+            pass
+    class Do:
+        def __init__(self):
+            pass
+
 
 
 
 
 if __name__ == '__main__':
-    regWhy = RegWhy()
-    print(regWhy.Make.talk())
-    print(regWhy.Make.Group.range())
-    print(regWhy.Make.Statement(["test","test2"]))
+    print(RegWhy.asterik)
+    print(RegWhy.CharacterType.anyCharacter)
